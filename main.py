76c6cbe47f316471ab56b315f9884978fee56792
Roni P <roni.palvaaho@gmail.com>
2017-08-13 19:12:05 +0300

    Optional arg for saving the model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import argparse
 from tensortest import *
 
 def import_data():
@@ -7,11 +8,18 @@ def import_data():
   return input_data.read_data_sets('MNIST_data', one_hot=True)
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--save', help='persist model after training (saved to vars/model.ckpt)',
+                      action='store_true')
+  args = parser.parse_args()
+
   with tf.Session() as sess:
     conv_net = ConvNetwork(sess)
     conv_net.build_graph()
 
     mnist = import_data()
     conv_net.train(mnist)
-    conv_net.save('vars/model.ckpt')
+
+    if args.save:
+      conv_net.save('vars/model.ckpt')
   
